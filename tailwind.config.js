aa2563bfe3ebee06b54a5fcbd7a6499eba5991a4
kureharyosuke <ohhayo.com@gmail.com>
2022-12-24 01:31:01 +0900

    setup: tailwind css setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
